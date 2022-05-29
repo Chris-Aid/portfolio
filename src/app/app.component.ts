28d63f68fad5a5a4aa4558c8d9a02f7eec7b16eb
Christian Aidelsburger <c.aidelsburger@gmx.de>
2022-05-29 10:28:48 +0200

    added image slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import { setInterval } from 'timers';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'christianaidelsburger';
+export class AppComponent implements OnInit {
+  images = ['firstimage.jpg', 'secondimage.jpg', 'thirdimage.jpg'];
+  currentImage = 0;
+  // showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      // this.showImage = false;
+
+      // setTimeout(() => {
+      //   this.showImage = true;
+      // }, 10);
+    }, 4000);
+  }
 }
